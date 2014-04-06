cf640e50f48d650c2bb535e5c024ceb0425e4d85
Les Pounder <lespounder@gmail.com>
2014-04-06 14:08:56 +0100

    Edited beacon.py to reflect the code better

--- beacon.py
+++ beacon.py
@@ -2,7 +2,7 @@
 from time import *
 import RPi.GPIO as GPIO
 import PiFm
-import os
+from piglow import PiGlow
 
 #These two variables control the pins used for our input, which is a button and for our output which is an LED.
 button_pin = 8
@@ -11,21 +11,13 @@ buzzer = 26
 #We setup the GPIO pins to use a logical numbering sequence, with the left column being odd numbers, and the right column being the even numbers.
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(button_pin , GPIO.IN)
-GPIO.setup(led_pin , GPIO.OUT)
 GPIO.setup(buzzer , GPIO.OUT)
-#Import the PiGlow module
-from piglow import PiGlow
 
 piglow = PiGlow()
 
 while True:
     if GPIO.input(button_pin)==1:
 	print("BUTTON PRESSED")
-        #GPIO.output(led_pin, True)
-        #sleep(2)
-        #GPIO.output(led_pin, False)
-        #sleep(2)
-        #os.system('sudo python /home/pi/piflash.py &')
         PiFm.play_sound("/home/pi/sound.wav")
         for i in range(0,3):
             GPIO.output(buzzer, True)
@@ -61,6 +53,24 @@ while True:
             GPIO.output(buzzer, True)
             piglow.all(128)
             sleep(1)
+            GPIO.output(buzzer, False)
+            piglow.all(0)
+            sleep(1)
+            GPIO.output(buzzer, True)
+            piglow.all(128)
+            sleep(0.5)
+            GPIO.output(buzzer, False)
+            piglow.all(0)
+            sleep(0.5)
+            GPIO.output(buzzer, True)
+            piglow.all(128)
+            sleep(0.5)
+            GPIO.output(buzzer, False)
+            piglow.all(0)
+            sleep(0.5)
+            GPIO.output(buzzer, True)
+            piglow.all(128)
+            sleep(0.5)
             GPIO.output(buzzer, False)
             piglow.all(0)
             sleep(1)
